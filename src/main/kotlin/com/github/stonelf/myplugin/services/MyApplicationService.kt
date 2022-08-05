d23f232a1fff4e3aba498e7ddbd1f88c2aaabc30
GitHub Action <action@github.com>
2022-08-05 06:34:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/stonelf/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stonelf/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stonelf.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stonelf.myplugin.MyBundle
 
 class MyApplicationService {
 
